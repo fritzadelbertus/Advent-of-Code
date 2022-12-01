746126494fb1561946e599b212f0e78f316a4ede
Fritz <furitsucode@gmail.com>
2022-12-01 19:40:51 +0700

    Day 1 - 2022
    Calorie Counting

---
+++ Day 1/Day1.py
@@ -0,0 +1,19 @@
+# Advent of Code Day 1: Calorie Counting
+
+f = open('Day 1/Day1.txt', 'r')
+thelongshit = f.read()
+arr = thelongshit.split('\n\n')
+listOfCalories = list(map(lambda x: x.split('\n'), arr))
+listOfTotalCalories = []
+for i in listOfCalories:
+  a = 0
+  for j in i:
+    a += int(j)
+  listOfTotalCalories.append(a)
+listOfTotalCalories.sort()
+elf1 = listOfTotalCalories[-1]
+print(f"The Elf with the most calories are {elf1}")
+elf2 = listOfTotalCalories[-2]
+elf3 = listOfTotalCalories[-3]
+print(f"The Elves which have the most calories are {elf1}, {elf2}, {elf3}")
+print(f"The sum of the Three Elves with most calories are { elf1 + elf2 + elf3 }")
---
+++ README.md
@@ -0,0 +1,4 @@
+# Advent of Code
+A Repository for my solutions during the Advent of Code Event
+
+All solutions are written in python
