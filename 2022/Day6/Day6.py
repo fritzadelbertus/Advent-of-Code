a73385ecd5bf55037517e687bf3209776962547d
Fritz <furitsucode@gmail.com>
2022-12-08 21:30:41 +0700

    Day 6, 2022
    Tuning Trouble

---
+++ 2022/Day6/Day6.py
@@ -0,0 +1,22 @@
+# Advent of Code Day 6: Tuning Trouble
+
+f = open('2022/Day6/Day6.txt', 'r')
+data = f.read()
+
+def findMarker(datastream, mark):
+  marker = 0
+  message = ''
+  for i in range(len(datastream)):
+    if datastream[i] in message:
+      message = message[message.find(datastream[i])+1:]
+    message += datastream[i]
+    if len(message) == mark:
+      marker = i 
+      break
+  return marker + 1
+
+packerMarker = findMarker(data, 4)
+messageMarker = findMarker(data, 14)
+
+print(f'{packerMarker} characters need to be processed before the first start-of-packet marker is detected')
+print(f'{messageMarker} characters need to be processed before the first start-of-message marker is detected')
