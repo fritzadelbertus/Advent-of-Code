5c4885ad6e4fa6e67094a99becaeac95b12b3006
Fritz <furitsucode@gmail.com>
2022-12-19 06:45:45 +0700

    Day 1, 2021
    Sonar Sweep

---
+++ 2021/Day1/Day1.py
@@ -0,0 +1,19 @@
+# Advent of Code Day 1: Sonar Sweep
+
+f = open('2021/Day1/Day1.txt', 'r')
+raw = f.read().split('\n')
+listOfDepth = list(map(lambda x: int(x), raw))
+
+# Part 1
+increasein1 = 0
+for i in range(1, len(listOfDepth)):
+  if listOfDepth[i] > listOfDepth [i-1]:
+    increasein1 += 1
+print(f'The number of times the depth increase is {increasein1}')
+
+# Part 2
+increasein3 = 0
+for i in range(3, len(listOfDepth)):
+  if listOfDepth[i] > listOfDepth [i-3]:
+    increasein3 += 1
+print(f'The number of times the sum is larger than the previous sum is {increasein3}')
